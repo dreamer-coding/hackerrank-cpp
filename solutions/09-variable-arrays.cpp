bd439c4e6440dc8bacab5280e14008006d02908e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-02-21 16:43:33 -0600

    Create 09-variable-arrays.cpp

---
+++ solutions/09-variable-arrays.cpp
@@ -0,0 +1,38 @@
+/*
+==============================================================================
+Author: Michael Gene Brockus (Dreamer)
+Email: michaelbrockus@gmail.com
+Organization: Fossil Logic
+Description: 
+    This file is part of the Fossil Logic project, where innovation meets
+    excellence in software development. Michael Gene Brockus, also known as
+    "Dreamer," is a dedicated contributor to this project. For any inquiries,
+    feel free to contact Michael at michaelbrockus@gmail.com.
+==============================================================================
+*/
+#include <iostream>
+#include <vector>
+
+int main() {
+    int length, query, size;
+    int index, element;
+    std::cin >> length >> query;
+
+    std::vector<std::vector<int>> arrays(length);
+
+    for (auto i = 0; i < length; i++) {
+        std::cin >> size;
+        arrays[i].resize(size);
+        for (auto j = 0; j < size; j++) {
+            std::cin >> arrays[i][j];
+        } // end for
+    } // end for
+
+    for (auto i = 0; i < query; i++) {
+        std::cin >> index >> element;
+        if (index >= 0 && index < length && element >= 0 && element < arrays[index].size()) {
+            std::cout << arrays[index][element] << std::endl;
+        } // end if
+    } // end for
+    return 0;
+} // end of main
