311e2c875acd9a4114688d35eec2634b6dfa5d91
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-02-21 15:26:28 -0600

    Update and rename 01-solution.cpp to 01-hello-world.cpp

--- solutions/01-hello-world.cpp
+++ solutions/01-hello-world.cpp
@@ -13,6 +13,6 @@ Description:
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, Fossil Logic!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 } // end of main
