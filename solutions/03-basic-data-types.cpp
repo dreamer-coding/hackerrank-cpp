294ccc0a3e9a97a3a8dd0a7dc559fdc4332aba26
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-02-21 15:40:21 -0600

    Create 03-basic-data-types.cpp

---
+++ solutions/03-basic-data-types.cpp
@@ -0,0 +1,33 @@
+/*
+==============================================================================
+Author: Michael Gene Brockus (Dreamer)
+Email: michaelbrockus@gmail.com
+Organization: Fossil Logic
+Description: 
+    This file is part of the Fossil Logic project, where innovation meets
+    excellence in software development. Michael Gene Brockus, also known as
+    "Dreamer," is a dedicated contributor to this project. For any inquiries,
+    feel free to contact Michael at michaelbrockus@gmail.com.
+==============================================================================
+*/
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    int number;
+    long large_number;
+    char letter;
+    float point;
+    double large_point;
+
+    std::cin >> number >> large_number;
+    std::cin >> letter;
+    std::cin >> point >> large_point;
+    
+    std::cout << number << std::endl;
+    std::cout << large_number << std::endl;
+    std::cout << letter << std::endl;
+    std::cout << fixed << std::setprecision(3) << point << std::endl;
+    std::cout << fixed << std::setprecision(9) << large_point << std::endl;
+    return 0;
+} // end of main
