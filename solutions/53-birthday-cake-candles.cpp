1ce5a1fbd42dfe916237e5d10f00c0b01f287cf6
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-04-30 10:18:30 -0600

    Update 53-birthday-cake-candles.cpp

--- solutions/53-birthday-cake-candles.cpp
+++ solutions/53-birthday-cake-candles.cpp
@@ -10,7 +10,10 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -74,50 +77,39 @@ auto main() -> int
 // file being used in this problem.
 //
 // arg-list:
-//  -> str: string refrence being passed in for left trim
+//  -> str: string reference being passed in for left trim
 //
 string ltrim(const string &str)
 {
     string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
+    s.erase(s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); }));
     return s;
-} // end of func
+}
 
 //
 // Should trim the data read in from the test case
 // file being used in this problem.
 //
 // arg-list:
-//  -> str: string refrence being passed in for right trim
+//  -> str: string reference being passed in for right trim
 //
 string rtrim(const string &str)
 {
     string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
+    s.erase(find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end());
     return s;
-} // end of func
+}
 
 //
 // Should split the data into tokens so they can be used
 // in the problem.
 //
 // arg-list:
-//  -> str: string refrence being passed in for left trim
+//  -> str: string reference being passed in for left trim
 //
 vector<string> split(const string &str)
 {
     vector<string> tokens;
-
     string::size_type start = 0;
     string::size_type end = 0;
 
@@ -125,9 +117,8 @@ vector<string> split(const string &str)
     {
         tokens.push_back(str.substr(start, end - start));
         start = end + 1;
-    } // end while
+    }
 
     tokens.push_back(str.substr(start));
-
     return tokens;
-} // end of func
+}
