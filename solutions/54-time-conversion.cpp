ba1831b62e859425ab9c0b5f88b60a7a3c756976
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-04-30 10:00:29 -0600

    Update 54-time-conversion.cpp

--- solutions/54-time-conversion.cpp
+++ solutions/54-time-conversion.cpp
@@ -10,7 +10,9 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include <bits/stdc++.h>
+#include <string>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -22,31 +24,21 @@ using namespace std;
 // arg-list:
 //  -> s: A single string thatrepresents a time
 //
-string timeConversion(string s)
-{
+string timeConversion(string s) {
     string ans;
     cin >> s;
 
-    if (s.substr(8, 8) == "AM")
-    {
-        if (s.substr(0, 2) == "12")
-        {
+    if (s.substr(8, 8) == "AM") {
+        if (s.substr(0, 2) == "12") {
             ans += "00";
-        } // end if
-        else
-        {
+        } else {
             ans += s.substr(0, 2);
         } // end else
 
-    } // end if
-    else
-    {
-        if (s.substr(0, 2) == "12")
-        {
+    } else {
+        if (s.substr(0, 2) == "12") {
             ans += "12";
-        } // end if
-        else
-        {
+        } else {
             auto hour = stoi(s.substr(0, 2));
             hour += 12;
             ans += to_string(hour);
@@ -62,8 +54,7 @@ string timeConversion(string s)
 //
 // main is where all C++ programs start
 //
-auto main() -> int
-{
+auto main() -> int {
     ofstream fout(getenv("OUTPUT_PATH"));
 
     string s;
