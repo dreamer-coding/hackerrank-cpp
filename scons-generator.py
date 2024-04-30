7400959856c020f79ed911916c3598660b0c8821
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-04-30 13:40:40 -0600

    Update scons-generator.py

--- scons-generator.py
+++ scons-generator.py
@@ -4,35 +4,29 @@ class SConsScriptGenerator:
     def __init__(self, project_name='YourProject'):
         self.project_name = project_name
 
-    def generate_source_build_script(self):
-        source_script = f"""
+    def generate_build_script(self):
+        script_content = """
+import os
+from SCons.Script import Environment, Glob, Default
+
 env = Environment(CC='gcc', CXX='g++', CFLAGS=['-std=c2x'], CXXFLAGS=['-std=c++20', '-Werror'], tools=[])
 
-Program('main', Glob('*.cpp'))
-Default('main')
-        """
-        with open('SConstruct', 'w') as file:
-            file.write(source_script)
+solutions_dir = 'solutions'
+solutions = [os.path.splitext(f)[0] for f in os.listdir(solutions_dir) if f.endswith('.cpp')]
 
-    def generate_subdirectory_build_script(self):
-        solutions_dir = 'solutions'
-        solutions = [os.path.splitext(f)[0] for f in os.listdir(solutions_dir) if f.endswith('.cpp')]
-        solved = len(solutions)
-    
-        subdirectory_script = f"""
-env = Environment(CC='gcc', CXX='g++', CFLAGS=['-std=c2x'], CXXFLAGS=['-std=c++20', '-Werror'], tools=[])
-
-solutions = {solutions}
-solved = {solved}
-
+programs = []
 for solution in solutions:
-    Program(target=solution, source=solution + '.cpp')
-        """
-        with open(os.path.join(solutions_dir, 'SConscript'), 'w') as file:
-            file.write(subdirectory_script)
+    target = os.path.join('bin', solution)  # Target executable path
+    source = os.path.join(solutions_dir, f"{solution}.cpp")  # Source file path
+    program = env.Program(target=target, source=source)
+    programs.append(program)
+
+Default(programs)  # Set the default target to be all the programs
+"""
+        with open('SConstruct', 'w') as file:
+            file.write(script_content)
 
 
 if __name__ == "__main__":
     scons_generator = SConsScriptGenerator(project_name='Hacker Rank')
-    scons_generator.generate_source_build_script()
-    scons_generator.generate_subdirectory_build_script()
+    scons_generator.generate_build_script()
