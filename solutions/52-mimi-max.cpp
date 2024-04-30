1b50ef36a6e0b079802857037b0ad9ab9e16abb3
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-04-30 10:17:19 -0600

    Update 52-mimi-max.cpp

--- solutions/52-mimi-max.cpp
+++ solutions/52-mimi-max.cpp
@@ -10,7 +10,10 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -40,30 +43,27 @@ void miniMaxSum(vector<int> arr)
         {
             max = num;
             min = num;
-        } // end if
+        }
         else
         {
             if (num > max)
             {
                 max = num;
-            } // end if
+            }
             else if (num < min)
             {
                 min = num;
-            } // end else if
-
-        } // end else
-
-    } // end for
-    std::cout << (sum - max) << " " << (sum - min) << std::endl;
-} // end of func
+            }
+        }
+    }
+    cout << (sum - max) << " " << (sum - min) << endl;
+}
 
 //
 // main is where all C++ programs start
 //
 auto main() -> int
 {
-
     string arr_temp_temp;
     getline(cin, arr_temp_temp);
 
@@ -74,64 +74,52 @@ auto main() -> int
     for (int i = 0; i < 5; i++)
     {
         int arr_item = stoi(arr_temp[i]);
-
         arr[i] = arr_item;
-    } // end for
+    }
 
     miniMaxSum(arr);
 
     return 0;
-} // end of func
+}
 
 //
 // Should trim the data read in from the test case
 // file being used in this problem.
 //
 // arg-list:
-//  -> str: string refrence being passed in for left trim
+//  -> str: string reference being passed in for left trim
 //
 string ltrim(const string &str)
 {
     string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
+    s.erase(s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); }));
     return s;
-} // end of func
+}
 
 //
 // Should trim the data read in from the test case
 // file being used in this problem.
 //
 // arg-list:
-//  -> str: string refrence being passed in for right trim
+//  -> str: string reference being passed in for right trim
 //
 string rtrim(const string &str)
 {
     string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
+    s.erase(find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end());
     return s;
-} // end of func
+}
 
 //
 // Should split the data into tokens so they can be used
 // in the problem.
 //
 // arg-list:
-//  -> str: string refrence being passed in for left trim
+//  -> str: string reference being passed in for left trim
 //
 vector<string> split(const string &str)
 {
     vector<string> tokens;
-
     string::size_type start = 0;
     string::size_type end = 0;
 
@@ -139,9 +127,8 @@ vector<string> split(const string &str)
     {
         tokens.push_back(str.substr(start, end - start));
         start = end + 1;
-    } // end while
+    }
 
     tokens.push_back(str.substr(start));
-
     return tokens;
-} // end of func
+}
