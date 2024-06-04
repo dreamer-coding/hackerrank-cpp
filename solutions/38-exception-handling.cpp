c06315ed27566c0c01cfbd17342545997d2ac2fa
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-06-04 16:08:17 -0600

    Create 38-exception-handling.cpp

---
+++ solutions/38-exception-handling.cpp
@@ -0,0 +1,49 @@
+/*
+==============================================================================
+Author: Michael Gene Brockus (Dreamer)
+Email: michaelbrockus@gmail.com
+Organization: Fossil Logic
+Description: 
+    This file is part of the Fossil Logic project, where innovation meets
+    excellence in software development. Michael Gene Brockus, also known as
+    "Dreamer," is a dedicated contributor to this project. For any inquiries,
+    feel free to contact Michael at michaelbrockus@gmail.com.
+==============================================================================
+*/
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int largest_proper_divisor(int n) {
+    if (n == 0) {
+        throw invalid_argument("largest proper divisor is not defined for n=0");
+    }
+    if (n == 1) {
+        throw invalid_argument("largest proper divisor is not defined for n=1");
+    }
+    for (int i = n/2; i >= 1; --i) {
+        if (n % i == 0) {
+            return i;
+        }
+    }
+    return -1; // will never happen
+}
+
+void process_input(int n) {
+    try {
+        int d = largest_proper_divisor(n);
+        cout << "result=" << d << endl;
+    } catch(const invalid_argument& e) {
+        cout << e.what() << endl; // I'd use cerr insted of cout
+    }
+    cout << "returning control flow to caller" << endl;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    process_input(n);
+    return 0;
+}
